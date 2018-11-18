ea19fe1874b4b2b2b006d79a577b01a0346ee7d0
Ignas Anikevicius <anikevicius@gmail.com>
2018-11-18 18:05:23 +0200

    Add view structures to encapsulate the view logic

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,80 @@
 use git2::Repository;
+use std::fmt::{self, Display, Formatter};
 
 type R<T> = Result<T, String>;
 
 fn main() {
-    println!("Hello, world!");
+    match display_prompt("") {
+        Ok(p) => print!("{} ", p),
+        Err(_) => print!(" "),
+    }
+}
+
+pub fn display_prompt(path: &str) -> R<PromptView> {
+    let repo = Repository::discover(path).or_else(|e| Err(format!("{:?}", e)))?;
+    let pr = prompt(&repo)?;
+    Ok(PromptView::new(pr))
+}
+
+pub struct PromptView {
+    pub repo: RepoStatusView,
+    pub branch: BranchStatusView,
+    pub local: LocalStatusView,
+}
+
+impl PromptView {
+    fn new(prompt: Prompt) -> PromptView {
+        PromptView {
+            repo: RepoStatusView { model: prompt.repo },
+            branch: BranchStatusView {
+                model: prompt.branch,
+            },
+            local: LocalStatusView {
+                model: prompt.local,
+            },
+        }
+    }
+}
+
+impl Display for PromptView {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.repo, self.branch, self.local)
+    }
+}
+
+pub struct RepoStatusView {
+    pub model: RepoStatus,
+}
+
+impl Display for RepoStatusView {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.model)
+    }
+}
+pub struct BranchStatusView {
+    pub model: BranchStatus,
+}
+
+impl Display for BranchStatusView {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.model)
+    }
+}
+pub struct LocalStatusView {
+    pub model: LocalStatus,
+}
+
+impl Display for LocalStatusView {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.model)
+    }
 }
 
 pub fn prompt(_repo: &Repository) -> R<Prompt> {
     Err("TODO".to_owned())
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Prompt {
     pub repo: RepoStatus,
     pub branch: BranchStatus,
@@ -21,13 +85,13 @@ pub fn repo_status(_repo: &Repository) -> R<RepoStatus> {
     Err("TODO".to_owned())
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct RepoStatus {
     pub branch: Option<String>,
     pub state: RepoState,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum RepoState {
     CherryPick,
     Merge,
@@ -41,7 +105,7 @@ pub fn branch_status(_repo: &Repository) -> R<BranchStatus> {
     Err("TODO".to_owned())
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct BranchStatus {
     pub ahead: usize,
     pub behind: usize,
@@ -51,7 +115,7 @@ pub fn local_status(_repo: &Repository) -> R<LocalStatus> {
     Err("TODO".to_owned())
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct LocalStatus {
     pub staged: usize,
     pub unmerged: usize,
