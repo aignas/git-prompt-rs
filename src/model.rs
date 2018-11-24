3f77e57eed869b4ccf7871e92700239a79dcbf5f
Ignas Anikevicius <anikevicius@gmail.com>
2018-11-24 08:56:11 +0200

    finish implementation of the prompt without colours

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+type RepoState = git2::RepositoryState;
+
 #[derive(Clone, Debug)]
 pub struct Prompt {
     pub repo: RepoStatus,
@@ -6,22 +8,11 @@ pub struct Prompt {
     pub local: LocalStatus,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct RepoStatus {
     // the branch might not be known, when the repo does not have any commits
     pub branch: Option<String>,
     pub state: RepoState,
-}
-
-#[derive(Clone, Debug)]
-pub enum RepoState {
-    New,
-    OK,
-
-    CherryPick,
-    Merge,
-    Rebase,
-    Revert,
 }
 
 #[derive(Clone, Debug, Default)]
@@ -29,10 +20,101 @@ pub struct BranchStatus {
     pub ahead: usize,
     pub behind: usize,
 }
-#[derive(Clone, Debug, Default)]
+
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct LocalStatus {
     pub staged: usize,
     pub unmerged: usize,
     pub unstaged: usize,
     pub untracked: usize,
+}
+
+impl LocalStatus {
+    pub fn increment(&mut self, status: git2::Status) {
+        if status.is_wt_new() {
+            self.untracked += 1;
+        }
+        if status.is_index_new()
+            || status.is_index_modified()
+            || status.is_index_deleted()
+            || status.is_index_renamed()
+            || status.is_index_typechange()
+        {
+            self.staged += 1
+        }
+        if status.is_wt_modified()
+            || status.is_wt_deleted()
+            || status.is_wt_renamed()
+            || status.is_wt_typechange()
+        {
+            self.unstaged += 1
+        }
+        if status.is_conflicted() {
+            self.unmerged += 1
+        }
+    }
+}
+
+#[cfg(test)]
+mod local_status {
+    use super::*;
+
+    fn get_status(s: git2::Status) -> LocalStatus {
+        let mut actual = LocalStatus {
+            ..Default::default()
+        };
+        actual.increment(s);
+        actual
+    }
+
+    #[test]
+    fn untracked() {
+        let expected = LocalStatus {
+            untracked: 1,
+            ..Default::default()
+        };
+        assert_eq!(get_status(git2::Status::WT_NEW), expected);
+    }
+    #[test]
+    fn staged() {
+        let expected = LocalStatus {
+            staged: 1,
+            ..Default::default()
+        };
+        assert_eq!(get_status(git2::Status::INDEX_NEW), expected);
+        assert_eq!(get_status(git2::Status::INDEX_MODIFIED), expected);
+        assert_eq!(get_status(git2::Status::INDEX_DELETED), expected);
+        assert_eq!(get_status(git2::Status::INDEX_RENAMED), expected);
+        assert_eq!(get_status(git2::Status::INDEX_TYPECHANGE), expected);
+    }
+    #[test]
+    fn unstaged() {
+        let expected = LocalStatus {
+            unstaged: 1,
+            ..Default::default()
+        };
+        assert_eq!(get_status(git2::Status::WT_MODIFIED), expected);
+        assert_eq!(get_status(git2::Status::WT_DELETED), expected);
+        assert_eq!(get_status(git2::Status::WT_RENAMED), expected);
+        assert_eq!(get_status(git2::Status::WT_TYPECHANGE), expected);
+    }
+    #[test]
+    fn conflict() {
+        let expected = LocalStatus {
+            unmerged: 1,
+            ..Default::default()
+        };
+        assert_eq!(get_status(git2::Status::CONFLICTED), expected);
+    }
+    #[test]
+    fn partial_stage() {
+        let expected = LocalStatus {
+            staged: 1,
+            unstaged: 1,
+            ..Default::default()
+        };
+        let mut status = git2::Status::WT_MODIFIED;
+        status.insert(git2::Status::INDEX_MODIFIED);
+        assert_eq!(get_status(status), expected);
+    }
 }
--- src/view.rs
+++ src/view.rs
@@ -3,6 +3,34 @@ use std::fmt::{self, Display, Formatter};
 
 pub struct Colors {}
 
+#[derive(Clone)]
+pub struct StatusSymbols {
+    pub nothing: char,
+    pub staged: char,
+    pub unmerged: char,
+    pub unstaged: char,
+    pub untracked: char,
+}
+
+const DEFAULT_STATUS_SYMBOLS: StatusSymbols = StatusSymbols {
+    nothing: '✔',
+    staged: '●',
+    unmerged: '✖',
+    unstaged: '✚',
+    untracked: '…',
+};
+
+#[derive(Clone)]
+pub struct BranchSymbols {
+    pub ahead: char,
+    pub behind: char,
+}
+
+const DEFAULT_BRANCH_SYMBOLS: BranchSymbols = BranchSymbols {
+    ahead: '↑',
+    behind: '↓',
+};
+
 pub struct PromptView {
     pub repo: RepoStatusView,
     pub branch: BranchStatusView,
@@ -13,15 +41,21 @@ impl PromptView {
     pub fn new(p: Prompt, _c: &Colors) -> PromptView {
         PromptView {
             repo: RepoStatusView { model: p.repo },
-            branch: BranchStatusView { model: p.branch },
-            local: LocalStatusView { model: p.local },
+            branch: BranchStatusView {
+                model: p.branch,
+                symbols: DEFAULT_BRANCH_SYMBOLS,
+            },
+            local: LocalStatusView {
+                model: p.local,
+                symbols: DEFAULT_STATUS_SYMBOLS,
+            },
         }
     }
 }
 
 impl Display for PromptView {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{} {} {}", self.repo, self.branch, self.local)
+        write!(f, "{}{} {}", self.repo, self.branch, self.local)
     }
 }
 
@@ -31,26 +65,216 @@ pub struct RepoStatusView {
 
 impl Display for RepoStatusView {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.model)
+        let s = match &self.model.state {
+            git2::RepositoryState::Merge => "merge",
+            git2::RepositoryState::Revert => "revert",
+            git2::RepositoryState::RevertSequence => "revert…",
+            git2::RepositoryState::CherryPick => "cherry-pick",
+            git2::RepositoryState::CherryPickSequence => "cherry-pick…",
+            git2::RepositoryState::Bisect => "bisect",
+            git2::RepositoryState::Rebase => "rebase",
+            git2::RepositoryState::RebaseInteractive => "rebase-i",
+            git2::RepositoryState::RebaseMerge => "rebase-m",
+            _ => "",
+        };
+        let maybe = if s == "" { None } else { Some(s) };
+
+        match (&self.model.branch, &maybe) {
+            (Some(a), Some(b)) => write!(f, "{} {}", a, b),
+            (Some(a), None) => write!(f, "{}", a),
+            (None, Some(b)) => write!(f, "{}", b),
+            (None, None) => Ok(()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod repo_status_view {
+    use super::*;
+
+    #[test]
+    fn nothing() {
+        let v = RepoStatusView {
+            model: RepoStatus {
+                branch: None,
+                state: git2::RepositoryState::Clean,
+            },
+        };
+        assert_eq!(format!("{}", v), "");
+    }
+
+    #[test]
+    fn branch_is_shown() {
+        let v = RepoStatusView {
+            model: RepoStatus {
+                branch: Some("master".to_owned()),
+                state: git2::RepositoryState::Clean,
+            },
+        };
+        assert_eq!(format!("{}", v), "master");
+    }
+
+    #[test]
+    fn rebase_is_shown() {
+        let v = RepoStatusView {
+            model: RepoStatus {
+                branch: Some("master".to_owned()),
+                state: git2::RepositoryState::Rebase,
+            },
+        };
+        assert_eq!(format!("{}", v), "master rebase");
     }
 }
 
 pub struct BranchStatusView {
     pub model: Option<BranchStatus>,
+    pub symbols: BranchSymbols,
 }
 
 impl Display for BranchStatusView {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.model)
+        match &self.model {
+            Some(b) => {
+                if b.ahead != 0 || b.behind != 0 {
+                    write!(f, " ");
+                }
+                non_zero(self.symbols.ahead, b.ahead, f)?;
+                non_zero(self.symbols.behind, b.behind, f)
+            }
+            None => Ok(()),
+        }
     }
 }
 
+#[cfg(test)]
+mod branch_status_view {
+    use super::*;
+
+    #[test]
+    fn none() {
+        let v = BranchStatusView {
+            model: None,
+            symbols: DEFAULT_BRANCH_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), "");
+    }
+
+    #[test]
+    fn up_to_date() {
+        let v = BranchStatusView {
+            model: Some(BranchStatus {
+                ahead: 0,
+                behind: 0,
+            }),
+            symbols: DEFAULT_BRANCH_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), "");
+    }
+
+    #[test]
+    fn ahead() {
+        let v = BranchStatusView {
+            model: Some(BranchStatus {
+                ahead: 6,
+                behind: 0,
+            }),
+            symbols: DEFAULT_BRANCH_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), " ↑6");
+    }
+
+    #[test]
+    fn behind() {
+        let v = BranchStatusView {
+            model: Some(BranchStatus {
+                ahead: 1,
+                behind: 3,
+            }),
+            symbols: DEFAULT_BRANCH_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), " ↑1↓3");
+    }
+}
+
+const LOCAL_CLEAN: LocalStatus = LocalStatus {
+    staged: 0,
+    unmerged: 0,
+    unstaged: 0,
+    untracked: 0,
+};
+
 pub struct LocalStatusView {
     pub model: LocalStatus,
+    pub symbols: StatusSymbols,
 }
 
 impl Display for LocalStatusView {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.model)
+        if LOCAL_CLEAN == self.model {
+            write!(f, "{}", self.symbols.nothing)
+        } else {
+            non_zero(self.symbols.unmerged, self.model.unmerged, f)?;
+            non_zero(self.symbols.staged, self.model.staged, f)?;
+            non_zero(self.symbols.unstaged, self.model.unstaged, f)?;
+            if self.model.untracked != 0 {
+                write!(f, "{}", self.symbols.untracked);
+            }
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod local_status_view {
+    use super::*;
+
+    #[test]
+    fn clean() {
+        let v = LocalStatusView {
+            model: LocalStatus {
+                staged: 0,
+                unmerged: 0,
+                unstaged: 0,
+                untracked: 0,
+            },
+            symbols: DEFAULT_STATUS_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), "✔");
+    }
+
+    #[test]
+    fn zeroes_are_omitted() {
+        let v = LocalStatusView {
+            model: LocalStatus {
+                staged: 1,
+                unmerged: 0,
+                unstaged: 0,
+                untracked: 4,
+            },
+            symbols: DEFAULT_STATUS_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), "●1…");
+    }
+
+    #[test]
+    fn not_clean() {
+        let v = LocalStatusView {
+            model: LocalStatus {
+                staged: 1,
+                unmerged: 2,
+                unstaged: 3,
+                untracked: 4,
+            },
+            symbols: DEFAULT_STATUS_SYMBOLS,
+        };
+        assert_eq!(format!("{}", v), "✖2●1✚3…");
+    }
+}
+
+fn non_zero(prefix: char, number: usize, f: &mut Formatter) -> fmt::Result {
+    if number != 0 {
+        write!(f, "{}{}", prefix, number)
+    } else {
+        Ok(())
     }
 }
